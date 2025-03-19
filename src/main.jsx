4c34fe18914e15b06cadd8461017f9fc6a4951e2
martin setbon <setbon.martin@gmail.com>
2025-03-19 17:02:57 +0900

    hey

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,7 @@
 import './index.css';
+import { createRoot } from 'react-dom/client';
+
+const rootContainer = document.getElementById("root");
+const root = createRoot( rootContainer);
+
+root.render(<h1>Hi Jason!</h1>)
